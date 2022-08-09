63c8986c754d5c6c0a8337e004f1e27e94598933
Artur <artur131996@gmail.com>
2022-08-09 17:34:44 +0300

    Add Sound Effects and Background Music

--- 2DPlatformerGame/Assets/Scripts/PlayerMovement.cs
+++ 2DPlatformerGame/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private enum MovementState { idle, running, jumping, falling}
 
+    [SerializeField] private AudioSource jumpSoundEffect;
 
     private void Start()
     {
@@ -34,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            jumpSoundEffect.Play();
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
